786bc4e2ecb13b52f701a969d01e6e68727470ee
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-10-08 22:22:43 +0530

    added the footer nav

---
+++ app/_components/NavBar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Link from "next/link";
+import { IoHomeOutline } from "react-icons/io5";
+import { MdOutlineMail } from "react-icons/md";
+import { FaGithub } from "react-icons/fa6";
+import { IoLogoLinkedin } from "react-icons/io5";
+import { FaXTwitter } from "react-icons/fa6";
+
+export const Navbar = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="w-full max-w-[400px] md:w-[400px] fixed bottom-2 bg-slate-600/40  flex justify-around rounded-lg p-3">
+        <div className="flex justify-between w-full p-1">
+          <Link href={"/"}>
+            <IoHomeOutline className="w-5 h-5 hover:scale-110 cursor-pointer transition-all" />
+          </Link>
+          <MdOutlineMail className="w-5 h-5 hover:scale-110 cursor-pointer transition-all" />
+          <Link href={"https://github.com/KoyalkarAditya"}>
+            <FaGithub className="w-5 h-5 hover:scale-110 cursor-pointer transition-all" />
+          </Link>
+          <Link href={"https://www.linkedin.com/in/koyalkar-aditya-9a1896260/"}>
+            <IoLogoLinkedin className="w-5 h-5 hover:scale-110 cursor-pointer transition-all" />
+          </Link>
+          <FaXTwitter className="w-5 h-5 hover:scale-110 cursor-pointer transition-all" />
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,17 @@ import { About } from "./_components/About";
 import { Experience } from "./_components/Experience";
 import { Skills } from "./_components/Skills";
 import { Projects } from "./_components/Projects";
+import { Navbar } from "./_components/NavBar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-5 pb-16">
       <Header />
       <About />
       <Experience />
       <Skills />
       <Projects />
+      <Navbar />
     </div>
   );
 }
