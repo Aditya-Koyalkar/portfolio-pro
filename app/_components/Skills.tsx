1059d8f2405e608a7f1af4512062cbd8d4fb7e2a
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-03-17 22:31:20 +0530

    fixes

--- app/_components/Skills.tsx
+++ app/_components/Skills.tsx
@@ -21,22 +21,11 @@ export const Skills = () => {
   return (
     <div className="flex flex-col gap-1">
       <div className="text-[22px] font-extrabold">Skills</div>
-      <motion.div
-        variants={containerVariants}
-        initial="hidden"
-        animate="visible"
-        className="grid grid-cols-4 md:grid-cols-8 gap-3"
-      >
+      <motion.div variants={containerVariants} initial="hidden" animate="visible" className="grid grid-cols-4 md:grid-cols-8 gap-3">
         {SkillData.map((skill, index) => (
-          <motion.div
-            key={index}
-            className="relative flex flex-col items-center group"
-            variants={itemVariants}
-          >
-            <div className="mb-2 text-slate-400 text-xs opacity-0 group-hover:opacity-100 transition-opacity">
-              {skill.name}
-            </div>
-            <Image src={skill.logo} height={50} width={50} alt={skill.name} />
+          <motion.div key={index} className="relative flex flex-col items-center group" variants={itemVariants}>
+            <div className="mb-2 text-slate-400 text-xs opacity-0 group-hover:opacity-100 transition-opacity">{skill.name}</div>
+            <Image src={skill.logo} alt={skill.name} className="object-cover w-10 h-10 " />
           </motion.div>
         ))}
       </motion.div>
