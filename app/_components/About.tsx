6d875e30dc1181f3f95acc80ffc17e1abcba1c9c
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-03-17 21:46:11 +0530

    added erino in experience

--- app/_components/About.tsx
+++ app/_components/About.tsx
@@ -3,10 +3,9 @@ export const About = () => {
     <div className="flex flex-col gap-1">
       <div className="text-[22px] font-extrabold">About</div>
       <div className="text-sm">
-        I&apos;m a Full Stack developer , dedicated to building and designing
-        scalable full stack applications . I have Interned at Al-Connect where I
-        initially started as a front-end developer before transitioning into a
-        full-stack role.
+        I&apos;m a Full Stack Developer dedicated to building and designing scalable full-stack applications. I am currently interning at Erino, where
+        I am building a CRM from scratch. Previously, I interned at early-stage startups like ProAnalyser and Al-Connect, where I contributed to the
+        development of their MVPs.
       </div>
     </div>
   );
--- app/_data/ExpInfo.ts
+++ app/_data/ExpInfo.ts
@@ -1,6 +1,19 @@
 import Proanalyser from "../../assets/proanalyser.jpeg";
 import AlConnect from "../../assets/alconnect.png";
+import Erino from "../../assets/erino.png";
 export const ExperienceInfo = [
+  {
+    role: "Software Engineering Intern",
+    company: "Erino",
+    description: `Developed advanced AG Grid implementations, optimizing performance and enhancing data visualization,supporting mltiple views of records.
+ Designed and developed a robust RBAC system ensuring secure access management across multiple user roles in
+ multiple teams.Created a seamless flow for inviting users and managing their registration process with email notifications and access
+ management.`,
+    image: Erino,
+    link: "https://erino.io/",
+    timeline: "Nov 2024 - Present",
+  },
+
   {
     role: "Frontend Developer - Contract",
     company: "ProAnalyser",
@@ -8,7 +21,7 @@ export const ExperienceInfo = [
       "Led the transition of the entire codebase from plain HTML to React, improving maintainability, performance, and scalability.Designed and developed modern, responsive, and user-friendly UIs based on provided designs, elevating the user experience across the platform. tegrated APIs seamlessly to connect frontend interfaces with backend services, ensuring smooth data flow and dynamic content rendering.Implemented full functionality for all features of the startup's product, from user interaction to data management.Optimized the code for production, resolving performance issues, and ensuring the application was production-ready for deployment.",
     image: Proanalyser,
     link: "https://webapp.proanalyser.in/",
-    timeline: "Sep 2024 - Current",
+    timeline: "Sep 2024 - Oct 2024",
   },
   {
     role: "Full-Stack Developer Intern",
