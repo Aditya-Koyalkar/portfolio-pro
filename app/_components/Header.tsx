38948d861a1c1d320d370a4495ca0a0fd9017380
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-03-17 22:06:50 +0530

    fixes

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -5,22 +5,11 @@ export const Header = () => {
   return (
     <div className="flex items-center justify-between gap-8">
       <div className="">
-        <div className="text-2xl font-bold">
-          {" "}
-          Hey, I&apos;m Aditya &#x1f44b;{" "}
-        </div>
-        <div className="text-sm mt-2 text-slate-400">
-          {/* Crafting seamless digital experiences as a full-stack wizard */}
-        </div>
+        <div className="text-2xl font-bold"> Hey, I&apos;m Aditya &#x1f44b; </div>
+        <div className="text-sm mt-2 text-slate-400">Crafting seamless digital experiences as a full-stack wizard</div>
       </div>
       <div>
-        <Image
-          alt=""
-          src={Profile}
-          height={100}
-          width={100}
-          className="rounded-full"
-        />
+        <Image alt="" src={Profile} height={100} width={100} className="rounded-full" />
       </div>
     </div>
   );
