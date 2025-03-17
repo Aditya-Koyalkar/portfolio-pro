714a84482d3cb09a20060f43198d18c43afc7383
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-03-17 21:54:49 +0530

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "./_components/ThemeProvider";
 import { GeistMono } from "geist/font/mono";
