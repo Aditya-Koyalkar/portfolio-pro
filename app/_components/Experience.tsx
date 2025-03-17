d9f00129eb49faee7602ab263975f43f81ccbd99
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-03-17 21:53:47 +0530

    fixes

--- app/_components/Experience.tsx
+++ app/_components/Experience.tsx
@@ -21,53 +21,32 @@ export const Experience = () => {
     hidden: { opacity: 0, y: 20 },
     visible: { opacity: 1, y: 0 },
   };
-  const [showFullDescription, setShowFullDescription] = useState(
-    Array(ExperienceInfo.length).fill(false)
-  );
-  const [showLink, setShowLink] = useState(
-    Array(ExperienceInfo.length).fill(false)
-  );
+  const [showFullDescription, setShowFullDescription] = useState(Array(ExperienceInfo.length).fill(false));
+  const [showLink, setShowLink] = useState(Array(ExperienceInfo.length).fill(false));
   const toggleDescription = (index: number) => {
-    setShowFullDescription((prevState) =>
-      prevState.map((item, i) => (i === index ? !item : item))
-    );
+    setShowFullDescription((prevState) => prevState.map((item, i) => (i === index ? !item : item)));
   };
 
   const handleMouseEnter = (index: number) => {
-    setShowLink((prevState) =>
-      prevState.map((item, i) => (i === index ? true : item))
-    );
+    setShowLink((prevState) => prevState.map((item, i) => (i === index ? true : item)));
   };
 
   const handleMouseLeave = (index: number) => {
-    setShowLink((prevState) =>
-      prevState.map((item, i) => (i === index ? false : item))
-    );
+    setShowLink((prevState) => prevState.map((item, i) => (i === index ? false : item)));
   };
 
   return (
     <div className="flex flex-col gap-1">
       <div className="text-[22px] font-extrabold">Experience</div>
-      <motion.div
-        variants={containerVariants}
-        initial="hidden"
-        animate="visible"
-        className="flex flex-col gap-3 mt-3"
-      >
+      <motion.div variants={containerVariants} initial="hidden" animate="visible" className="flex flex-col gap-3 mt-3">
         {ExperienceInfo.map((experience, index) => {
           const truncatedDescription = experience.description.slice(0, 200);
           const isLongDescription = experience.description.length > 200;
 
           return (
-            <motion.div variants={itemVariants} key={index} className="flex">
-              <div className="p-3">
-                <Image
-                  src={experience.image}
-                  height={50}
-                  width={50}
-                  alt=""
-                  className="rounded-full"
-                />
+            <motion.div variants={itemVariants} key={index} className="flex items-start">
+              <div className="px-3 py-1">
+                <Image src={experience.image} height={50} width={50} alt="" className="rounded-full" />
               </div>
               <div className="w-full flex flex-col gap-3">
                 <div className="flex justify-between">
@@ -79,28 +58,16 @@ export const Experience = () => {
                       onMouseEnter={() => handleMouseEnter(index)}
                       onMouseLeave={() => handleMouseLeave(index)}
                     >
-                      {experience.company}{" "}
-                      {showLink[index] && (
-                        <span className=" cursor-pointer font-bold">&gt;</span>
-                      )}
+                      {experience.company} {showLink[index] && <span className=" cursor-pointer font-bold">&gt;</span>}
                     </Link>
-                    <div className="text-[12px] md:text-sm">
-                      {experience.role}
-                    </div>
+                    <div className="text-[12px] md:text-sm">{experience.role}</div>
                   </div>
-                  <div className="text-sm text-slate-400">
-                    {experience.timeline}
-                  </div>
+                  <div className="text-sm text-slate-400">{experience.timeline}</div>
                 </div>
                 <div className="text-sm">
-                  {showFullDescription[index]
-                    ? experience.description
-                    : truncatedDescription + (isLongDescription ? "..." : "")}
+                  {showFullDescription[index] ? experience.description : truncatedDescription + (isLongDescription ? "..." : "")}
                   {isLongDescription && (
-                    <span
-                      className="text-slate-500 ml-1 underline cursor-pointer"
-                      onClick={() => toggleDescription(index)}
-                    >
+                    <span className="text-slate-500 ml-1 underline cursor-pointer" onClick={() => toggleDescription(index)}>
                       {showFullDescription[index] ? " Show Less" : " Show More"}
                     </span>
                   )}
