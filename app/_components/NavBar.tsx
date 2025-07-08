3a329841290dd4bfab237dc078e3033b34b76a2c
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-07-09 01:15:28 +0530

    fixes

--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -13,16 +13,16 @@ export const Navbar = () => {
     <div className="flex justify-center">
       <div className="w-full max-w-[250px] min md:w-[400px] fixed bottom-2 bg-slate-600/40  flex justify-around rounded-lg p-3">
         <div className="flex justify-between w-full p-1">
-          <Link href={"/"}>
+          <Link href={"/"} target="_blank">
             <IoHomeOutline className="w-5 h-5 hover:scale-110 cursor-pointer transition-all" />
           </Link>
-          <a href="mailto:koyalkaraditya123@gmail.com">
+          <a href="mailto:koyalkaraditya123@gmail.com" target="_blank">
             <MdOutlineMail className="w-5 h-5 hover:scale-110 cursor-pointer transition-all" />
           </a>{" "}
-          <Link href={"https://github.com/Aditya-Koyalkar"}>
+          <Link href={"https://github.com/Aditya-Koyalkar"} target="_blank">
             <FaGithub className="w-5 h-5 hover:scale-110 cursor-pointer transition-all" />
           </Link>
-          <Link href={"https://www.linkedin.com/in/aditya-koyalkar/"}>
+          <Link href={"https://www.linkedin.com/in/aditya-koyalkar/"} target="_blank">
             <IoLogoLinkedin className="w-5 h-5 hover:scale-110 cursor-pointer transition-all" />
           </Link>
           {/* <FaXTwitter className="w-5 h-5 hover:scale-110 cursor-pointer transition-all" /> */}
